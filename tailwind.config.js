38f8e0ee039de7b979730422114d3fd5e9e1105a
Tanqir Masroor <tanqir.msarker@gmail.com>
2021-11-17 14:13:30 +0600

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily : {
+      sans: ['Manrope', '-apple-system', 'BlinkMacSystemFont', 'sans-serif'],
+      serif: ['Manrope', '-apple-system', 'BlinkMacSystemFont', 'serif']
+    },
     extend: {},
   },
   variants: {
