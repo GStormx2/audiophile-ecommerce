0c713eb397872423dde7b574a38667f70009ebaa
Tanqir Masroor <tanqir.msarker@gmail.com>
2021-11-17 19:27:22 +0600

    testing push

--- components/FeaturedProduct.jsx
+++ components/FeaturedProduct.jsx
@@ -6,13 +6,15 @@ import headerImage from '../assets/home/mobile/image-header.jpg';
 const FeaturedProduct = () => {
     
     return (
-        <section className="relative flex justify-center items-center text-center">
+        <section className="relative flex justify-center items-center">
             <Image src={headerImage} alt="Headphone" />
-            <div className="absolute z-10">
-                <p className="uppercase text-overline text-gray-600">new product</p>
-                <h1 className="text-white text-[36px] leading-[] font-bold uppercase">xx99 mark II headphones</h1>
-                <p className="text-white text-body text-md">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast</p>
-                <Button type={'ternary'}>shop</Button>
+            <div className="absolute z-10 top-52 text-center">
+              <div className="px-6">
+                <p className="uppercase text-overline text-gray-500 mb-3 ">new product</p>
+                <h1 className="text-white text-[36px] leading-[40px] tracking-[1.29px] font-bold uppercase mb-4">xx99 mark II headphones</h1>
+                <p className="text-body text-gray-400 mb-5">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast</p>
+                <Button type={'primary'}>see product</Button>
+              </div>
             </div>
         </section>
     );
