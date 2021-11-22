ab1b90805cdd2861ec2d4523508d69f280bc413e
Tanqir Masroor <tanqir.msarker@gmail.com>
2021-11-22 16:23:46 +0600

    moved logo, menus inside flex in LG to fix centering issue

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,19 +9,25 @@ const Header = () => {
   return (
     <header className="absolute left-1/2 top-14 transform -translate-x-1/2 -translate-y-1/2 z-20 h-[90px] w-full max-w-[1200px] border-b-[1px] border-gray-700 lg:h-[100px] lg:px-10">
       <div className="flex justify-between items-center h-full px-6 md:px-10 lg:px-0">
+        {/* hamburger icon */}
         <div className="md:hidden block">
           <Image src={hamburgerSVG} alt="menu" />
         </div>
-        <div className="md:hidden block lg:block">
-          <Image className="md:hidden" src={logoSVG} alt="logo" />
+        {/* audiophile logo */}
+        <div className="md:hidden block lg:hidden">
+          <Image src={logoSVG} alt="logo" />
         </div>
-        <div className="hidden lg:block">
+        {/* menus in large screen */}
+        <div className="hidden lg:flex jusitify-between items-center gap-48">
+          <Image src={logoSVG} alt="logo" />
           <Menu />
         </div>
+        {/* container for hamburger, logo in small to medium screen */}
         <div className="md:flex md:justify-between md:items-center md:gap-10 hidden lg:hidden">
           <Image className="" src={hamburgerSVG} alt="menu" />
           <Image src={logoSVG} alt="logo" />
         </div>
+        {/* cart logo */}
         <Image src={cartSVG} alt="cart" />
       </div>
     </header>
